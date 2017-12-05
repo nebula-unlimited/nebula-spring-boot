2424785994d8473e072fe13912c215d9d326262f
Cloud <66999882@qq.com>
2017-12-05 14:15:36 +0800

    Update

--- Spring-Boot-MongoDB/src/main/java/com/nebula/web/IndexController.java
+++ Spring-Boot-MongoDB/src/main/java/com/nebula/web/IndexController.java
@@ -13,7 +13,22 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index() {
+        // 创建三个 User 并验证 User 总数
         userRepository.save(new User(1L, "nebula", 30));
+        userRepository.save(new User(2L, "aaa", 20));
+        userRepository.save(new User(3L, "bbb", 10));
+        System.out.println(userRepository.findAll().size());
+
+        // 删除一个 User 输出 User 总数
+        User user = userRepository.findOne(2L);
+        userRepository.delete(user);
+        System.out.println(userRepository.findAll().size());
+
+        // 再删除一个 User 输出 User 总数
+        user = userRepository.findByUsername("bbb");
+        userRepository.delete(user);
+        System.out.println(userRepository.findAll().size());
+
         return "ok";
     }
 }
