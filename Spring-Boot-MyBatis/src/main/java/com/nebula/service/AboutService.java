d9f0f9a6960b0fbd7b703c30aad07e3e141459c1
Cloud <66999882@qq.com>
2017-12-18 02:27:50 +0800

    Update Comment.

--- Spring-Boot-MyBatis/src/main/java/com/nebula/service/AboutService.java
+++ Spring-Boot-MyBatis/src/main/java/com/nebula/service/AboutService.java
@@ -11,18 +11,54 @@ import java.util.List;
  */
 
 public interface AboutService {
-
+    /**
+     * 查找全部记录
+     * @param about
+     * @return
+     */
     List<About> findAll(About about);
 
+    /**
+     * 分页查找
+     * @param page
+     * @param size
+     * @return
+     */
     List<About> findPage(int page, int size);
 
+    /**
+     * 查找一条记录
+     * @param id
+     * @return
+     */
     About findOne(int id);
 
+    /**
+     * 根据名称查找记录
+     * @param name
+     * @return
+     */
     About findByName(String name);
 
+    /**
+     * 创建
+     * @param name
+     * @param content
+     * @return
+     */
     Long insert(String name, String content);
 
+    /**
+     * 更新
+     * @param about
+     * @return
+     */
     int update(About about);
 
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
     int delete(Long id);
 }
